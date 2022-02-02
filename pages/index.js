a9ba05c7548df18b6d411e925b4d195759cceca6
Sam Marxz <sammarxzz@gmail.com>
2022-02-02 18:07:25 -0300

    create Stats Components

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import TwitterStats from "../components/twitter-stats";
+import InstagramStats from "../components/instagram-stats";
+import YouTubeStats from "../components/youtube-stats";
+
 export default function Home() {
   return (
-    <div className=''>
-      <h1 className='text-3xl text-red-500 font-bold'>Hello World</h1>
+    <div className="p-8">
+      <h3 className="text-lg font-medium leading-6 text-gray-900">Dashboard</h3>
+
+      <div className="grid grid-cols-1 gap-5 mt-5 lg:grid-cols-3">
+        <TwitterStats />
+        <YouTubeStats />
+        <InstagramStats />
+      </div>
     </div>
   );
 }
